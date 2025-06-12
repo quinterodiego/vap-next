abbb0482dd533de1237caaebef9fdf7e57c389e1
Diego Quintero <d86webs@gmail.com>
2025-06-12 20:51:31 -0300

    change path inngest client

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,6 +1,6 @@
 import { serve } from "inngest/next";
 import { inngest } from "@config/inngest";
-import { syncUserCreate, syncUserUpdate, syncUserDelete } from "@/config/inngest";
+import { inngest } from "../../../inngest/client";
 
 // Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
