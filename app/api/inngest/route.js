3771ca590d48ec057ef195d22782b57550ddcd34
Diego Quintero <d86webs@gmail.com>
2025-06-12 23:02:18 -0300

    change path inngest client

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,12 +1,12 @@
-import { inngest, syncUserCreate, syncUserDelete, syncUserUpdate } from "@/config/inngest";
+import { inngest, syncUserCreation, syncUserDeletion, syncUserUpdation } from "@/config/inngest";
 import { serve } from "inngest/next";
 
 // Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [
-    syncUserCreate,
-    syncUserUpdate,
-    syncUserDelete
+    syncUserCreation,
+    syncUserUpdation,
+    syncUserDeletion
   ],
 });
